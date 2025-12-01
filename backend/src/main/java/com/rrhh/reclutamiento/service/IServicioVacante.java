d81d9055f1c61a7d04cb2355bd4c6aaf10bd4fe8
Andrey Hegel Zafra Venegas <131420439+Andrey3210@users.noreply.github.com>
2025-12-01 16:38:35 -0500

    Filter public vacancies by process stage

--- backend/src/main/java/com/rrhh/reclutamiento/service/IServicioVacante.java
+++ backend/src/main/java/com/rrhh/reclutamiento/service/IServicioVacante.java
@@ -2,6 +2,7 @@ package com.rrhh.reclutamiento.service;
 
 import com.rrhh.shared.domain.model.Vacante;
 import com.rrhh.shared.domain.enums.EstadoVacante;
+import com.rrhh.shared.domain.enums.EtapaProceso;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ public interface IServicioVacante {
     Vacante publicarVacante(Integer idVacante);
     Vacante cerrarVacante(Integer idVacante);
     List<Vacante> buscarVacantesActivas();
+    List<Vacante> buscarVacantesPorEstadoYEtapa(EstadoVacante estado, EtapaProceso etapa);
     Vacante actualizarVacante(Integer idVacante, Vacante vacante);
     Vacante obtenerVacantePorId(Integer idVacante);
     void eliminarVacante(Integer idVacante);
